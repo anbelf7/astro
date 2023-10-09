45b5b39826e0d960f9a16f2e51728b5cb56de672
Andrea Belfiore <35812320+anbelf7@users.noreply.github.com>
2023-10-09 06:58:24 +0200

    Update

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,5 @@
 document.getElementById("hamburger").addEventListener("click", () => {
+  // animazione menu
   // per usare multipli toggle
   ["-space-y-1", "space-y-1", "py-4"].map((e) =>
     document.getElementById("hamburger").classList.toggle(e),
@@ -7,7 +8,9 @@ document.getElementById("hamburger").addEventListener("click", () => {
   document.getElementById("ham2").classList.toggle("hidden");
   document.getElementById("ham3").classList.toggle("-rotate-45");
 
+  // menu mobile
   ["hidden", "flex", "flex-col", "gap-y-5", "mt-6"].map((e) =>
     document.getElementById("menu").classList.toggle(e),
   );
+  // document.getElementById("ricerca").classList.toggle("hidden");
 });
