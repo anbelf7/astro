8378e4277de00df15afd94ce5f76401eb33db4f3
Andrea Belfiore <35812320+anbelf7@users.noreply.github.com>
2024-10-04 14:54:48 +0100

    Modifico menu mobile

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,5 +1,5 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase("https://abelfiore.pockethost.io/");
+const pb = new PocketBase(import.meta.env.POCKETBASE_URL);
 
 export default pb;
